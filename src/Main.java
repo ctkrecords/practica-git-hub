119795e7e05b7184aa1663b458cfb5fcdd48a250
ctkrecords <cesarmejia1194@gmail.com>
2024-09-03 20:26:54 -0600

    Add new variable miTexto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String miTexto = "Welcome to Java Developer";
+        System.out.println("Hi! " + miTexto);
+        //System.out.println("Hello world!");
     }
 }
